bf600788a6e8f42c7951ece25ba58b19bfa15944
frostghost555 <83512260+frostghost555@users.noreply.github.com>
2024-09-11 14:36:18 -0700

    Final spirograph.py
    I was able to figure out how to add color to my pattern.

--- spirograph.py
+++ spirograph.py
@@ -1,27 +1,28 @@
+#turtle setup
 import turtle as trtl
-
-
 screen = trtl.Screen()
 screen.bgcolor("white")
 pen = trtl.Turtle()
 pen.speed("fastest")
 
+#This is the list of colors, for the spirograph.
+rainbow_colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+#size square and coordinates
 size = 800
 x = 0
 y = 0
 
 
-for i in range(50):
+for i in range(50):   #pen moves diagonally from the top left corner
     pen.up()
     pen.goto(x - size / 2, y + size / 2)
     pen.down()
-    
-    for _ in range(4):
+    pen.color(rainbow_colors[i % len(rainbow_colors)]) #colors the squares in a pattern, based on the original list.
+    for _ in range(4):  #creates the squares
         pen.forward(size)
         pen.right(90)
-    
-    # Update size and move to the next square
     size -= 16
 
-pen.hideturtle()  # Hide the turtle after drawing
-screen.mainloop()  # Keep the window open
+pen.hideturtle() 
+screen.mainloop() 
